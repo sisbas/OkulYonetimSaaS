0c1e7c2f6b0bd0a7c721e7333ce5ae8378de04cc
SEMİH İŞBAŞ <sisbas@yahoo.com>
2026-07-05 19:50:28 +0300

    Add phase 1 permission keys

--- src/database/seeds/roles.seed.ts
+++ src/database/seeds/roles.seed.ts
@@ -1,1 +1,6 @@
-export { ROLE_PERMISSION_SEED as ROLE_SEED } from './permissions.seed';
+import { ROLE_PERMISSION_SEED } from './permissions.seed';
+
+export const ROLE_SEED = Object.entries(ROLE_PERMISSION_SEED).map(([code, permissions]) => ({
+  code,
+  permissions: [...permissions],
+}));
