c78a954d7f02271f559f9844becb4422e90518d1
SEMİH İŞBAŞ <sisbas@yahoo.com>
2026-07-20 15:23:31 +0300

    feat(frontend): restore operations exports on latest main

---
+++ src/frontend/operations/index.ts
@@ -0,0 +1,5 @@
+export * from './operations.acceptance';
+export * from './operations.api-dependencies';
+export * from './operations.routes';
+export * from './operations.skeleton';
+export * from './operations.states';
