bd33fd9f103afa2c17694ff35f6b3e2462f5bcaf
SEMİH İŞBAŞ <sisbas@yahoo.com>
2026-08-05 01:24:21 +0300

    Verify locked browser dependencies in E2E workflow

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,9 @@
-module.exports = { moduleFileExtensions: ['js', 'json', 'ts'], rootDir: '.', testRegex: '.*\\.spec\\.ts$', transform: { '^.+\\.(t|j)s$': 'ts-jest' }, collectCoverageFrom: ['src/**/*.(t|j)s'], testEnvironment: 'node' };
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/.worktrees/'],
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  testEnvironment: 'node',
+};
